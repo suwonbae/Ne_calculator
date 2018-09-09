3b0f8171cebe244a4572d420f2018b4463f1326d
Suwon Bae <tg827589@login3.stampede2.tacc.utexas.edu>
2018-09-09 18:46:37 -0500

    edited postprocess.py

--- postprocess.py
+++ postprocess.py
@@ -1,15 +1,20 @@
 import numpy
 import subprocess
 
-lee=numpy.zeros(27)
-lpp=numpy.zeros(27)
-for i in range(27):
+num_chain=27
+num_atoms_per_mol=2000
+
+lee=numpy.zeros(num_chain)
+lpp=numpy.zeros(num_chain)
+for i in range(num_chain):
    subprocess.Popen("tail -2 stdout."+str(i+1)+" | head -1 > temp",shell=True).wait()
    result=numpy.loadtxt('temp')
    lee[i]=result[0]**2
    lpp[i]=result[1]
 
-bpp=numpy.mean(lpp)/(2000-1)
+subprocess.Popen("rm temp",shell=True).wait()
+
+bpp=numpy.mean(lpp)/(num_atoms_per_mol-1)
 app=numpy.mean(lee)/numpy.mean(lpp)
 Ne=app/bpp
 
